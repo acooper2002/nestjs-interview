cc2649d3a9d8b9c4c8774eba20adbb00c234de64
acooper2002 <ac253469@fi365.ort.edu.uy>
2025-05-27 12:57:10 -0300

    feat: Add TodoItemsController and integrate with TodoListsModule

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,11 @@
 import type { NestExpressApplication } from '@nestjs/platform-express';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
-
+  app.useGlobalPipes(new ValidationPipe({ transform: true }));
   await app.listen(3000);
 }
 
