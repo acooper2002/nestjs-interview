4c26bd90c7407c73a737b94af4489240deb53db5
acooper2002 <ac253469@fi365.ort.edu.uy>
2025-06-02 10:27:55 -0300

    fix: Export TodoItemsService and TodoListsService from their respective modules

--- src/todo_lists/todo_lists.module.ts
+++ src/todo_lists/todo_lists.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TodoListsController } from './todo_lists.controller';
 import { TodoListsService } from './todo_lists.service';
-import { SharedModule } from 'src/shared/shared.module';
+import { SharedModule } from '../shared/shared.module';
 
 @Module({
   imports: [SharedModule],
   controllers: [TodoListsController],
   providers: [TodoListsService],
+  exports: [TodoListsService],
 })
 export class TodoListsModule {}
